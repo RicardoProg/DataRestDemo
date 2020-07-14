f5310c68a5e073aa327e8fb8fdafddb74986abab
Ricardo <ricardo.miguel888@gmail.com>
2020-07-14 18:05:22 +0100

    data restresource only api

---
+++ src/main/java/com/ricardo/demo/CustomersRepo.java
@@ -0,0 +1,9 @@
+package com.ricardo.demo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel="customers", path="customers")
+public interface CustomersRepo extends JpaRepository<Customers, Integer>{
+
+}
